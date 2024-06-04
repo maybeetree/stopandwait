8cce82b936078e72316006982daec7c50b08c08f
maybetree <maybetree48@proton.me>
2024-06-04 16:34:26 +0200

    Fix experiments

--- experiments.sh
+++ experiments.sh
@@ -12,14 +12,14 @@ done
 echo "p1, iters" > dat/e2.csv
 for i in $(seq 0 0.01 1 | head -n -1)
 do
-	echo -n "${i}, " >> dat/e1.csv
+	echo -n "${i}, " >> dat/e2.csv
 	seq 200 | shuf | ./snw $i 0 | wc -l >> dat/e2.csv
 done
 
-echo "p, iters" > dat/e2.csv
+echo "p, iters" > dat/e3.csv
 for i in $(seq 0 0.01 1 | head -n -1)
 do
-	echo -n "${i}, " >> dat/e1.csv
+	echo -n "${i}, " >> dat/e3.csv
 	seq 100 | shuf | ./snw $i $i | wc -l >> dat/e3.csv
 done
 
